2188e2542d63bf1012372f4df4d0d6a55eb8043f
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2022-05-29 16:06:53 -0300

    correção organização arquivos

--- ATV_Listar_dados/CRUD-01-R/conf/script_bd.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `marca` (
-  `codigo` int(11) NOT NULL AUTO_INCREMENT,
-  `descricao` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
